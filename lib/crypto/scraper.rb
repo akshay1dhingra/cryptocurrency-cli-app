554103805af015afc40acaf19edce70283586935
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-02-26 17:54:41 -0600

    refactor assessment - called self.new_coin_scrape at the end of 1st meth

--- lib/crypto/scraper.rb
+++ lib/crypto/scraper.rb
@@ -4,7 +4,7 @@ class Crypto::Scraper
     doc = Nokogiri::HTML(open("https://coinranking.com/"))
 
     #create an array of all the coins along with their attributes set that to a variable
-    coins = doc.css(".coin-list__body .coin-list__body__row").collect do |row|
+    @coins = doc.css(".coin-list__body .coin-list__body__row").collect do |row|
       each_coin = row.css("span").collect do |text|
         text.text
         #binding.pry
@@ -14,14 +14,19 @@ class Crypto::Scraper
         each_coin << page2.css(".coin-page__description").text
       #binding.pry
     end
+    self.new_coin_scrape
+  end
 
-    #iterate through coins variable and take an individual coin and assign each of its attributes to their corresponding attributes
-    coins.each do |coin|
+
+  def self.new_coin_scrape
+
+      #iterate through coins variable and take an individual coin and assign each of its attributes to their corresponding attributes
+    @coins.each do |coin|
       #binding.pry
-      new_coin = Crypto::Coin.new(name = coin[2], price = coin[3], market_cap = coin[6], change = coin[9].gsub("\t","").gsub("\n",""), description = coin[11])
-      Crypto::Coin.all << new_coin #@all_coins << new_coin
+    new_coin = Crypto::Coin.new(name = coin[2], price = coin[3], market_cap = coin[6], change = coin[9].gsub("\t","").gsub("\n",""), description = coin[11])
+    Crypto::Coin.all << new_coin #@all_coins << new_coin
     end
-  Crypto::Coin.all
+    Crypto::Coin.all
   end
 
 end
